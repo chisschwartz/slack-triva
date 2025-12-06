c678074ede036a58a2c49a5e336d64d12f9dc917
chisschwartz <christopherschwartz559@gmail.com>
2025-12-06 13:43:08 -0600

    groundwork of what I may need for new bot

---
+++ slack_trivia.py
@@ -0,0 +1,34 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask, request, Response
+import slack.errors
+from slackeventsapi import SlackEventAdapter
+import string
+from datetime import datetime, timedelta
+import time
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'],'/slack/events', app)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']
+
+#schedule messages once a day, either give four choices/true false
+#respond 8-16 hours later with correct response/maybe dm user if they got correct or not
+#respond with correct answer after a time period always
+
+#MVP: bot posts once a day from a database, not repeating itself, 
+#then gives an answer some hours later. Aim for 10 questions for decent sample size
+
+#if time/energy allows make bot multiple choice. 
+#Bind choices to question, or have only correct bound with 3 random choices?
+
+#pick a random triva question to import to scheduling
+#keep log of last 1-5 questions to avoid repeats
+#maybe loop through all questions once, then restart with full question list
