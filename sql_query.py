745bba16284f46de0f6ccfab529aee1bf7bcdb1a
Chris Schwartz <christopherschwartz559@gmail.com>
2026-02-12 11:01:37 -0600

    trying to loop through each Q&A

--- sql_query.py
+++ sql_query.py
@@ -1,8 +1,9 @@
 import sql_database
 
 question = "SELECT Qid, Questions, Aid FROM trivia"
+question_answer = "SELECT Aid, Answers FROM answer"
 storage_id = []
-query_id = 1
+query_id = 0
 
 mycursor = sql_database.mydb.cursor()
 
@@ -21,11 +22,27 @@ class question:
         for query in mycursor:
             storage_id.append((query[0]))
             question_filter = query[1]
+            query_id = query[0]
+            
+        if query_id in storage_id:
+            if query_id == query[2]:
+                print(storage_id)
+                print(query_id)
+                return question_filter
     
-            if query_id in storage_id:
-                if query_id == query[2]:
-                    print(storage_id)
-                    print(query_id)
-                    return question_filter
-    
-    print(question_maker())            
+    print(question_maker())
+
+    # def answer():
+
+    #     answer_id = 0
+
+    #     mycursor.execute(question_answer)
+
+    #     for answer in mycursor:
+    #         answer_id = answer[0]
+    #         answer_filter = answer[1]
+
+    #         if answer_id in storage_id:
+    #             if answer_id == query_id:
+    #                 return answer_filter
+    # print(answer())
